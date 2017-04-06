fe95bf044f0ec3fb49e9da09e91db69a6323e242
Hideyuki TAKEUCHI <chimerast@gmail.com>
2017-04-07 01:30:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Slack Stamp Bot
+====
+
+Slack Stamp Bot enables stamping emoji on Slack.
+
+[![license](https://img.shields.io/github/license/chimerast/slack-stamp.svg)](LICENSE)
+
+TBD
+
+## Usage
+
+```
+/stamp :kemono-sugoi:
+```
+
+## License
+
+[Apache-2.0](https://github.com/chimerast/slack-stamp/blob/master/LICENCE)
+
+## Author
+
+[Hideyuki Takeuchi (@chimerast)](https://github.com/chimerast)
---
+++ bot.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+
+if (!process.env.SLACK_API_TOKEN) {
+  console.error('Error: SLACK_API_TOKEN is not specified')
+  process.exit(1)
+}
+
+const Botkit = require('botkit')
+const async = require('async')
+
+const controller = Botkit.slackbot({})
+
+const checkError = (err) => {
+  if (!err) return
+
+  console.error(err)
+  process.exit(1)
+}
+
+let botname = 'stamp'
+
+const rtm = controller.spawn({
+  token: process.env.SLACK_API_TOKEN
+}).startRTM((err, bot) => {
+  checkError(err)
+  botname = bot.identity.name
+})
+
+controller.setupWebserver(process.env.PORT, (err, webserver) => {
+  checkError(err)
+  controller.createWebhookEndpoints(webserver)
+})
+
+controller.on('slash_command', (bot, message) => {
+  async.parallel({
+    user: rtm.api.users.info.bind(null, { user: message.user_id }),
+    emoji: rtm.api.emoji.list.bind(null, {})
+  }, (err, res) => {
+    if (err) return bot.replyPrivate(message, err)
+
+    const profile = res.user.user.profile || {}
+    const emoji = res.emoji.emoji || {}
+
+    const attachments = message.text.match(/:[^:]+:/g)
+      .map(s => emoji[s.replace(/:/g, '')])
+      .filter(url => url)
+      .map(url => ({
+        color: '#fff',
+        text: '',
+        image_url: url
+      }))
+
+    if (attachments.length === 0) return bot.replyPrivate(message, 'Error: Typed emoji was not found.')
+
+    const reply = {
+      channel: message.channel,
+      username: profile.real_name,
+      icon_url: profile.image_72,
+      attachments: attachments
+    }
+
+    rtm.send(reply, function (err, res) {
+      if (err === 'channel_not_found') return bot.replyPrivate(message, 'Error: This channel is private. Please run command `/invite @' + botname + '`.')
+      if (err) return bot.replyPrivate(message, err)
+
+      bot.replyAcknowledge()
+    })
+  })
+})
